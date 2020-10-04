66d2e61aad095a33c38d06206b5ac4cd1ded4f7e
cfbonner <cfbonner@gmail.com>
2020-10-04 18:04:37 +0100

    Add first reflex :-)
    - Form updates on change

---
+++ app/reflexes/skater_reflex.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class SkaterReflex < ApplicationReflex
+  # Add Reflex methods in this file.
+  #
+  # All Reflex instances expose the following properties:
+  #
+  #   - connection - the ActionCable connection
+  #   - channel - the ActionCable channel
+  #   - request - an ActionDispatch::Request proxy for the socket connection
+  #   - session - the ActionDispatch::Session store for the current visitor
+  #   - url - the URL of the page that triggered the reflex
+  #   - element - a Hash like object that represents the HTML element that triggered the reflex
+  #   - params - parameters from the element's closest form (if any)
+  #
+  # Example:
+  #
+  #   def example(argument=true)
+  #     # Your logic here...
+  #     # Any declared instance variables will be made available to the Rails controller and view.
+  #   end
+  #
+  # Learn more at: https://docs.stimulusreflex.com
+
+  def updateTricks
+    @skater ||= Skater.find(params[:id])
+    @skater.update(skater_params)
+    @skater.save
+  end
+
+  private
+
+  def skater_params
+    params.require(:skater).permit(:name, :status, moves: [])
+  end
+end
