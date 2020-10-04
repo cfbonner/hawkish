80d9cbb36ab8b56ab7f3f280a7517c89f16c667e
cfbonner <cfbonner@gmail.com>
2020-10-04 19:36:41 +0100

    Add skater to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :skater
+  after_create :create_user_skater
+
+  private
+
+  def create_user_skater
+    Skater.create(user_id: id, name: email, status: new_skater_status)
+  end
+
+  def new_skater_status
+    [
+      'Lets go get some footy',
+      'It\'s rippin time',
+      'I am the passenger',
+      'Wake, Bake, Skate'
+    ].sample
+  end
 end
